43a14cc1600cc6e8fb461e59c689a80c3fa3feba
Pisit Nakjai <mynameisbee@gmail.com>
2019-02-17 08:27:24 +0700

    x

--- ESP_netpie_linebot/ESP_netpie_linebot.ino
+++ ESP_netpie_linebot/ESP_netpie_linebot.ino
@@ -2,15 +2,15 @@
 #include <MicroGear.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-const char* ssid     = "your SSID"; //change this to your SSID
-const char* password = "your PASSWORD"; //change this to your PASSWORD
+const char* ssid     = "beebrain"; //change this to your SSID
+const char* password = "1234554321"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* host = "https://beecontrolgate.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+#define APPID   "GateControl"     //change this to your APPID
+#define KEY     "CyjMAcrElwMZM53"     //change this to your KEY
+#define SECRET  "kA0orrqNFfCV2MEaoCuSxhir0"     //change this to your SECRET
 
-#define ALIAS   "NodeMCU1" //set name of drvice
+#define ALIAS   "Gate" //set name of drvice
 #define TargetWeb "switch" //set target name of web
 
 WiFiClient client;
--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'ALPkDPxL0IeyXrTkJmwwYyfRSGsRp4HCK2BHdwNtSUl21oCdgrX2o3zkeGU0yv9eTJ7Wu1KHxQ9E8THiccaC+mjKLdQYIoXEknO2fOmVkEJxt5nNqXHnJcbJcgePqyVhSWs5ZL9muLlulOa7xAg1IQdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Uabf6ae83f9b44fec0a7ff7c1ab0eca4f',
         'messages' => [$messages],
       ];
       $post = json_encode($data);

b9314f7988601aaefe9ae06c34242be385769ee1
Pisit Nakjai <mynameisbee@gmail.com>
2019-02-17 08:28:28 +0700

    x

--- bot.php
+++ bot.php
@@ -27,14 +27,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 
-			$Topic = "NodeMCU1" ;
+			$Topic = "Gate1" ;
 			getMqttfromlineMsg($Topic,$text);
 			   
 			
 		}
 	}
 }
-$Topic = "NodeMCU1" ;
+$Topic = "Gate1" ;
 $text = "Test";
 getMqttfromlineMsg($Topic,$text);
 echo "OK3";

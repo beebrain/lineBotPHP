d8283a98fe9e73b732d1c31d0bdbb395375b7dc5
beebrain <mynameisbee@gmail.com>
2019-02-18 08:14:08 +0700

    aa

--- webhooks.php
+++ webhooks.php
@@ -53,7 +53,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 			
-			$payload = [
+			$payload = 
 					{
 					  "type": "text",
 					  "text": "Quick Reply!",
@@ -70,8 +70,7 @@ if (!is_null($events['events'])) {
 							}
 						]
 					  }
-					}
-				];
+					};
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
